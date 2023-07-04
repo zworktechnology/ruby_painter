7e9d8c391c16a82c5432f3a3094799d1f9baae68
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-07-05 00:21:26 +0530

    Mobile menu

--- footer.php
+++ footer.php
@@ -107,7 +107,7 @@
             <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
 
             <div class="logo-box">
-                <a href="index.html" aria-label="logo image"><img src="assets/images/resources/footer-logo.png" width="155" alt=""></a>
+                <a href="index.php" aria-label="logo image"><img src="assets/images/logo/ruby_png.png" width="155" alt=""></a>
             </div>
             <!-- /.logo-box -->
             <div class="mobile-nav__container"></div>
@@ -116,19 +116,17 @@
             <ul class="mobile-nav__contact list-unstyled">
                 <li>
                     <i class="fa fa-envelope"></i>
-                    <a href="mailto:needhelp@packageName__.com">needhelp@ambed.com</a>
+                    <a href="mailto:info@rubypainters.com">info@rubypainters.com</a>
                 </li>
                 <li>
                     <i class="fa fa-phone-alt"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
+                    <a href="tel:+91 9842869957">+91 9842869957</a>
                 </li>
             </ul><!-- /.mobile-nav__contact -->
             <div class="mobile-nav__top">
                 <div class="mobile-nav__social">
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-facebook-square"></a>
-                    <a href="#" class="fab fa-pinterest-p"></a>
-                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="https://www.facebook.com/rubypainterstrichy" target="_blank" class="fab fa-facebook-square"></a>
+                    <a href="https://www.instagram.com/ruby_painters/" target="_blank" class="fab fa-instagram"></a>
                 </div><!-- /.mobile-nav__social -->
             </div><!-- /.mobile-nav__top -->
 
